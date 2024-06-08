1b84526acaf15653aac4035c59fa3d5df1b9f0ce
anand <anand.vishwag@gmail.com>
2024-06-08 19:20:48 +0530

    content and code changes

--- pages/admin/settings.js
+++
@@ -1,9 +0,0 @@
-// pages/user/dashboard.js
-import withAuth from '../../utils/withAuth';
-import AdminLayout from '../../components/layouts/AdminLayout';
-
-const AdminSettings= ({ user }) => {
-  return <div>Welcome, {user.name} to User Dashboard</div>;
-};
-
-export default withAuth(AdminSettings, 'user', AdminLayout);
